d87e6a33657580c664067ac3fd86c94dc676949a
Javierrasyadi <javierrasyadiii@gmail.com>
2023-06-22 11:23:01 +0700

    ubah

--- - Fajar/login.php
+++ - Fajar/login.php
@@ -9,7 +9,10 @@ $query_sql = "SELECT * FROM user WHERE email = '$email' AND password = '$passwor
 $result = mysqli_query($koneksi, $query_sql);
 $row = $result -> fetch_assoc();
 
-if(mysqli_num_rows($result) > 0){
+if($email=="admin@gmail.com"&& $password=="admin"){
+  header("Location: ../tampilanadmin/iniadmin_copy.php");
+
+}elseif(mysqli_num_rows($result) > 0){
     session_start();
     $_SESSION['email']=$email;
     $_SESSION['foto'] =$row['foto'];
