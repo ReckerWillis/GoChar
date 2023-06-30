35060e988d200c83d7b527f600fbf17cdde8d719
NurhalizaFAZ <21523167@students.uii.ac.id>
2023-06-30 13:54:18 +0700

    update

--- tampilanadmin/iniadmin_copy.php
+++ tampilanadmin/iniadmin_copy.php
@@ -220,6 +220,92 @@
           </div>
             <!-- </div> -->
       </section>
+      //chart kategori
+      <main id="main">
+    <!-- ======= About Section ======= -->
+    <section id="user" class="user">
+      <div class="container">
+        <div class="section-title">
+          <div class="card">
+            <div class="card-body">
+              <?php
+              include('conf/koneksi.php');
+      //Ambil data kategori dari tabel galang_donasi
+              $query = mysqli_query($koneksi, "SELECT kategori FROM galang_donasi");
+
+              // Buat array untuk menyimpan jumlah kategori
+              $kategoriCount = array();
+
+              // Menghitung jumlah kategori
+              while ($row = mysqli_fetch_assoc($query)) {
+                $kategori = $row['kategori'];
+                if (array_key_exists($kategori, $kategoriCount)) {
+                  $kategoriCount[$kategori]++;
+                } else {
+                  $kategoriCount[$kategori] = 1;
+                }
+              }
+
+              // Menghasilkan data dalam format yang sesuai untuk visualisasi
+              $labels = array_keys($kategoriCount);
+              $data = array_values($kategoriCount);
+              ?>
+
+              <!-- Tampilkan grafik menggunakan Chart.js -->
+              <canvas id="myChart"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End About Section -->
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    <!-- Script untuk membuat grafik menggunakan Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+      var labels = <?php echo json_encode($labels); ?>;
+      var data = <?php echo json_encode($data); ?>;
+
+      // Membuat grafik batang menggunakan Chart.js
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Jumlah',
+            data: data,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            y: {
+              beginAtZero: true,
+              stepSize: 1
+            }
+          }
+        }
+      });
+    </script>
+
       <!-- End nampilin Chart -->
       <!-- ======= Galang Donasi ======= -->
       <section id="gd" class="gd">
