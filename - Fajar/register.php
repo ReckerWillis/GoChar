6bfe39b400afc7212a2c1b9ec19423ed5405fea1
ReckerWillis <suryaramadhan850@gmail.com>
2023-07-07 21:04:26 +0700

    perbaikan login dan ubah password

--- - Fajar/register.php
+++ - Fajar/register.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query_sql = "INSERT INTO user (email, password, username) VALUES ('$email','$username','$password')";
+$query_sql = "INSERT INTO user (email, username,password) VALUES ('$email','$username','$password')";
 
 if(mysqli_query($koneksi, $query_sql)){
     header("Location: login.html");
